2ab0442043bef94b11ad7c8b04e7fe2adece151a
Szymon <szbuchowicz@gmail.com>
2024-06-15 19:51:48 +0200

    juz wszystko ;D

--- alt_main.c
+++ alt_main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "struktury.h"
 
+//WCZYTYWANIE DANYCH
+
 int wczytanie_z_pliku(char* src_file, Rezerwacja *lista_rezerwacji){
     FILE *plik;
     plik = fopen(src_file, "r");
@@ -10,8 +12,114 @@ int wczytanie_z_pliku(char* src_file, Rezerwacja *lista_rezerwacji){
         fscanf(plik, "%d %d %d %d", &lista_rezerwacji[i].id, &lista_rezerwacji[i].sala_rezerwowana.nr_sali, &lista_rezerwacji[i].data_rezerwacji.dzien, &lista_rezerwacji[i].data_rezerwacji.h);
         i++;
     }
+    fclose(plik);
     return i;
 }
+
+//1. REZERWOWANIE SALI
+
+int wolna_sala(Rezerwacja nowa,  Rezerwacja *rezerwacje, int liczba_rezerwacji){
+    for(int i = 0; i<liczba_rezerwacji; i++){
+        if(nowa.data_rezerwacji.dzien == rezerwacje[i].data_rezerwacji.dzien && nowa.data_rezerwacji.h == rezerwacje[i].data_rezerwacji.h && nowa.sala_rezerwowana.nr_sali == rezerwacje[i].sala_rezerwowana.nr_sali){
+            printf("Ta sala jest juz zarezerwowana o tej godzinie\n");
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int sala_istnieje(Rezerwacja nowa){
+    FILE *baza_sal;
+    Sala lista_sal[10];
+    int istnieje = 0;
+    baza_sal = fopen("sale.txt", "r");
+    int i = 0;
+    while (!feof(baza_sal))
+    {
+        fscanf(baza_sal, "%d", &lista_sal[i].nr_sali);
+        if(nowa.sala_rezerwowana.nr_sali == lista_sal[i].nr_sali){
+            istnieje = 1;
+            break;
+        }
+        i++;
+    }
+    return istnieje;
+}
+
+void drukowanie_rezerwacji(Rezerwacja nowa){
+    FILE *plik;
+    plik = fopen("paragon.txt", "w");
+    fprintf(plik, "*************\n");
+    fprintf(plik, "Rezerwacja %2d\n", nowa.id);
+    fprintf(plik, "  Sala: %3d\n", nowa.sala_rezerwowana.nr_sali);
+    fprintf(plik, "  Dzień: %2d\n", nowa.data_rezerwacji.dzien);
+    fprintf(plik, " Godzina: %2d\n", nowa.data_rezerwacji.h);
+    fprintf(plik, "*************\n");
+    fclose(plik);
+}
+
+int nowa_rezerwacja(Rezerwacja *lista_rezerwacji, int liczba_rezerwacji) {
+
+    Rezerwacja nowa;
+    nowa.id = lista_rezerwacji[liczba_rezerwacji-1].id + 1;
+
+    printf("Podaj dzien, kiedy chcesz zarezerwowac sale: ");
+    if (scanf("%d", &nowa.data_rezerwacji.dzien) != 1 || nowa.data_rezerwacji.dzien > 30 || nowa.data_rezerwacji.dzien < 1) {
+        printf("Zla data\n\n");
+        return liczba_rezerwacji;
+    }
+
+    printf("Podaj godzine, kiedy chcesz zarezerwowac sale: ");
+    if (scanf("%d", &nowa.data_rezerwacji.h) != 1 || nowa.data_rezerwacji.h > 16 || nowa.data_rezerwacji.h < 10) {
+        printf("Zla godzina\n\n");
+        return liczba_rezerwacji;
+    }
+
+    printf("Podaj numer sali, ktora chcesz zarezerwowac: ");
+    if (scanf("%d", &nowa.sala_rezerwowana.nr_sali) != 1) {
+        printf("\nNie ma mozliwosci wybrania tej sali\n\n");
+        return liczba_rezerwacji;
+    }
+    
+    if(wolna_sala(nowa, lista_rezerwacji, liczba_rezerwacji) == 0){
+        return liczba_rezerwacji;
+    }
+    if (sala_istnieje(nowa) == 0){
+        printf("\nPodana sala nie istnieje\n");
+        return liczba_rezerwacji;
+    }
+    
+
+    lista_rezerwacji[liczba_rezerwacji] = nowa;
+    liczba_rezerwacji++;
+  
+    printf("\nID rezerwacji: %d\nZarezerwowales sale: %d, dnia %d, o godzinie %d\n",nowa.id, nowa.sala_rezerwowana.nr_sali, nowa.data_rezerwacji.dzien, nowa.data_rezerwacji.h);
+
+    drukowanie_rezerwacji(nowa);
+    return liczba_rezerwacji;
+}
+
+//2. ANULOWANIE REZERWACJI
+
+int anuluj_rezerwacje(Rezerwacja *rezerwacje, int liczba_rezerwacji) {
+    int id_rez = 0;
+    printf("Podaj id rezerwacji ktora chcesz usunac: ");
+    scanf("%d", &id_rez);
+    printf("\n");
+    for (int i = 0; i < liczba_rezerwacji; i++) {
+        if (rezerwacje[i].id == id_rez) {
+            for (int j = i; j < liczba_rezerwacji - 1; j++) {
+                rezerwacje[j] = rezerwacje[j + 1];
+            }
+            liczba_rezerwacji--;
+            
+            break;
+        }
+    }
+    return liczba_rezerwacji;
+}
+
+//3. POKAŻ WSZYSTKIE REZERWACJE
 
 void pokaz_wszystkie_rezerwacje(Rezerwacja *lista_rezerwacji, int liczba_rezerwacji) {
     if (liczba_rezerwacji == 0) {
@@ -28,6 +136,8 @@ void pokaz_wszystkie_rezerwacje(Rezerwacja *lista_rezerwacji, int liczba_rezerwa
                lista_rezerwacji[i].data_rezerwacji.h);
     }
 }
+
+//4. POKAŻ REZERWACJE Z DANEGO DNIA
 
 void pokaz_rezerwacje_z_dnia(Rezerwacja *lista_rezerwacji, int liczba_rezerwacji) {
     int dzien;
@@ -54,96 +164,7 @@ void pokaz_rezerwacje_z_dnia(Rezerwacja *lista_rezerwacji, int liczba_rezerwacji
     }
 }
 
-int wolna_sala(Rezerwacja nowa,  Rezerwacja *rezerwacje, int liczba_rezerwacji){
-    for(int i = 0; i<liczba_rezerwacji; i++){
-        if(nowa.data_rezerwacji.dzien == rezerwacje[i].data_rezerwacji.dzien && nowa.data_rezerwacji.h == rezerwacje[i].data_rezerwacji.h && nowa.sala_rezerwowana.nr_sali == rezerwacje[i].sala_rezerwowana.nr_sali){
-            printf("Ta sala jest juz zarezerwowana o tej godzinie\n");
-            return 0;
-        }
-    }
-    return 1;
-}
-
-int nowa_rezerwacja(Rezerwacja *lista_rezerwacji, int liczba_rezerwacji) {
-
-    Rezerwacja nowa;
-    nowa.id = lista_rezerwacji[liczba_rezerwacji-1].id + 1;
-
-    printf("Podaj dzien, kiedy chcesz zarezerwowac sale: ");
-    if (scanf("%d", &nowa.data_rezerwacji.dzien) != 1 || nowa.data_rezerwacji.dzien > 30 || nowa.data_rezerwacji.dzien < 1) {
-        printf("Zla data\n\n");
-        return liczba_rezerwacji;
-    }
-
-    printf("Podaj godzine, kiedy chcesz zarezerwowac sale: ");
-    if (scanf("%d", &nowa.data_rezerwacji.h) != 1 || nowa.data_rezerwacji.h > 16 || nowa.data_rezerwacji.h < 10) {
-        printf("Zla godzina\n\n");
-        return liczba_rezerwacji;
-    }
-
-    printf("Podaj numer sali, ktora chcesz zarezerwowac: ");
-    if (scanf("%d", &nowa.sala_rezerwowana.nr_sali) != 1) {
-        printf("Nie ma mozliwosci wybrania tej sali\n\n");
-        return liczba_rezerwacji;
-    }
-    
-    if(wolna_sala(nowa, lista_rezerwacji, liczba_rezerwacji) == 0){
-        return liczba_rezerwacji;
-    }
-
-    lista_rezerwacji[liczba_rezerwacji] = nowa;
-    liczba_rezerwacji++;
-  
-    printf("\nID rezerwacji: %d\nZarezerwowales sale: %d, dnia %d, o godzinie %d\n",nowa.id, nowa.sala_rezerwowana.nr_sali, nowa.data_rezerwacji.dzien, nowa.data_rezerwacji.h);
-
-    return liczba_rezerwacji;
-}
-
-void usun_rezerwacje_z_pliku(const char* nazwa_pliku, int id_rez) {
-    FILE* plik = fopen(nazwa_pliku, "r");
-    FILE* tmp = fopen("tmp.txt", "w");
-    if (plik != NULL && tmp != NULL) {
-        char linia[256];
-        while (fgets(linia, sizeof(linia), plik)) {
-            int rez_id, sala_id, dzien, godzina;
-            if (sscanf(linia, "%d %d %d %d/n",
-                       &rez_id, &sala_id, &dzien, &godzina) == 4) {
-                if (!(rez_id == id_rez)) {
-                    fputs(linia, tmp);
-                }
-            }
-        }
-        fclose(plik);
-        fclose(tmp);
-        remove(nazwa_pliku);
-        rename("tmp.txt", nazwa_pliku);
-        printf("Usunięto rezerwację z pliku %s\n", nazwa_pliku);
-    } else {
-        printf("Błąd przy usuwaniu rezerwacji z pliku\n");
-    }
-}
-
-
-int anuluj_rezerwacje(Rezerwacja *rezerwacje, int liczba_rezerwacji) {
-    int id_rez = 0;
-    printf("Podaj id rezerwacji ktora chcesz usunac: ");
-    scanf("%d", &id_rez);
-    printf("\n");
-    for (int i = 0; i < liczba_rezerwacji; i++) {
-        if (rezerwacje[i].id == id_rez) {
-            for (int j = i; j < liczba_rezerwacji - 1; j++) {
-                rezerwacje[j] = rezerwacje[j + 1];
-            }
-            liczba_rezerwacji--;
-            
-            // Dodatkowo usuwamy rezerwację z pliku
-            usun_rezerwacje_z_pliku("rezerwacje.txt", id_rez);
-            
-            break;
-        }
-    }
-    return liczba_rezerwacji;
-}
+// ZAPIS DO PLIKU
 
 void zapis_rezerwacji_do_pliku_txt(Rezerwacja *rezerwacje, const char* nazwa_pliku, int liczba_rezerwacji) {
     FILE* plik = fopen(nazwa_pliku, "w");
@@ -157,9 +178,10 @@ void zapis_rezerwacji_do_pliku_txt(Rezerwacja *rezerwacje, const char* nazwa_pli
     } else {
         printf("Błąd przy zapisie do pliku\n");
     }
+    fclose(plik);
 }
 
-
+//MENU
 
 int main() {
     int wybor;
--- test.c
+++ test.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include "struktury.h"
 
-void DUPA()
-{
-    printf("DUPA!!! \n\n");
+int sala_istnieje(int nr_sali){
+    printf("dupa");
+    FILE *baza_sal;
+    Sala lista_sal[10];
+    int istnieje = 0;
+    baza_sal = fopen("sale.txt", "r");
+    int i = 0;
+    while (!feof(baza_sal))
+    {
+        fscanf("%d", &lista_sal[i].nr_sali);
+        printf("%d\n", lista_sal[i].nr_sali);
+        if(nr_sali == lista_sal[i].nr_sali){
+            istnieje = 1;
+            break;
+        }
+        i++;
+    }
+    return istnieje;
+}
+
+int main(){
+    Rezerwacja nowa;
+    nowa.id = 1;
+    nowa.data_rezerwacji.dzien = 12;
+    nowa.data_rezerwacji.h = 12;
+    nowa.sala_rezerwowana.nr_sali = 102;
+
+    printf("%d", sala_istnieje(102));
 }
